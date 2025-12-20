35971d180a0aab3e5919bbbeaf7f180f4b870cdf
Kai Law <lawkai@vivo.com>
2025-12-20 20:52:32 +0800

    No log

--- kernel/src/sync/mutex.rs
+++ kernel/src/sync/mutex.rs
@@ -130,7 +130,7 @@ impl Mutex {
             self as *const _
         );
         let mut w = self.pending.irqsave_lock();
-        //#[cfg(debugging_scheduler)]
+        #[cfg(debugging_scheduler)]
         crate::trace!(
             "[PEND_FOR] TH:0x{:x} has got the spinlock of mutex {:?}",
             Thread::id(&this_thread),
@@ -148,7 +148,7 @@ impl Mutex {
                 let ok = guard.add_acquired_mutex(this_mutex.clone());
                 debug_assert!(ok);
                 let _ = guard.replace_pending_on_mutex(None);
-                //#[cfg(debugging_scheduler)]
+                #[cfg(debugging_scheduler)]
                 crate::trace!(
                     "TH:0x{:x} @ P{} has got mutex {:?}",
                     Thread::id(&this_thread),
